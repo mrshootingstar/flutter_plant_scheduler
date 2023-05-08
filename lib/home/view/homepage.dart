286560cf176707f938e49e86c9ee709f3549f145
Shahab Mir <m.mirghorbani@gmail.com>
2023-05-07 17:32:51 -0700

    fullscreen camera works. but I need tofigure out my own UI

--- lib/home/view/homepage.dart
+++ lib/home/view/homepage.dart
@@ -1,4 +1,19 @@
+import 'package:better_open_file/better_open_file.dart';
+import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+Future<String> path(CaptureMode captureMode) async {
+  final Directory extDir = await getTemporaryDirectory();
+  final testDir =
+      await Directory('${extDir.path}/test').create(recursive: true);
+  final String fileExtension = captureMode == CaptureMode.photo ? 'jpg' : 'mp4';
+  final String filePath =
+      '${testDir.path}/${DateTime.now().millisecondsSinceEpoch}.$fileExtension';
+  return filePath;
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -40,17 +55,20 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: CameraAwesomeBuilder.awesome(
+          saveConfig: SaveConfig.photoAndVideo(
+            photoPathBuilder: () => path(CaptureMode.photo),
+            videoPathBuilder: () => path(CaptureMode.video),
+            initialCaptureMode: CaptureMode.photo,
+          ),
+          enablePhysicalButton: true,
+          filter: AwesomeFilter.AddictiveRed,
+          flashMode: FlashMode.auto,
+          aspectRatio: CameraAspectRatios.ratio_16_9,
+          previewFit: CameraPreviewFit.fitWidth,
+          onMediaTap: (mediaCapture) {
+            OpenFile.open(mediaCapture.filePath);
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
